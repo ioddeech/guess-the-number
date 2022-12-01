5ee31a6901f8a6da3e405856b510f67813cb982a
MIKITA IODA <nikitaiodo201@gmail.com>
2022-12-01 10:07:49 +0300

    value range

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int value = new Random().nextInt(100);
+        int value = new Random().nextInt(1000);
         String answer = "";
         while (true) {
             int attempt = new Scanner(System.in).nextInt();
