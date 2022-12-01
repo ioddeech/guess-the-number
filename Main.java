a686c08a44ce8ec0f93525abc9d6af7416c08c4d
MIKITA IODA <nikitaiodo201@gmail.com>
2022-12-01 10:04:13 +0300

    initial

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int value = new Random().nextInt(100);
+        String answer = "";
+        while (true) {
+            int attempt = new Scanner(System.in).nextInt();
+            if (value < attempt) {
+                System.out.println("Загаданное число меньше!");
+            } else if (value > attempt) {
+                System.out.println("Загаданное число больше!");
+            } else {
+                System.out.println("Вы угадали!");
+                break;
+            }
+        }
+    }
+}
